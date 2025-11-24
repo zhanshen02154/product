5a9ec48af34c007ff5e6d0146535d7ec47068cd6
wangjiawei1995 <zhanshen02154@gmail.com>
2025-11-24 12:32:01 +0800

    refactor(接口层): 更改Handler的初始化方式并使用对象池
    resolves: #31, #32

--- internal/intefaces/handler/product.go
+++ internal/intefaces/handler/product.go
@@ -2,14 +2,20 @@ package handler
 
 import (
 	"context"
+	"fmt"
 	"github.com/zhanshen02154/product/internal/application/dto"
 	"github.com/zhanshen02154/product/internal/application/service"
 	"github.com/zhanshen02154/product/pkg/swap"
 	"github.com/zhanshen02154/product/proto/product"
+	"go-micro.dev/v4/errors"
+	"net/http"
+	"sync"
 )
 
 type ProductHandler struct {
 	ProductApplicationService service.IProductApplicationService
+	objPool                   sync.Pool
+	orderInvetoryPool         sync.Pool
 }
 
 // AddProduct
@@ -21,7 +27,11 @@ type ProductHandler struct {
 //	@param resp
 //	@return error
 func (h *ProductHandler) AddProduct(ctx context.Context, req *product.ProductInfo, resp *product.ResponseProduct) error {
-	productAdd := &dto.ProductDto{}
+	productAdd := h.objPool.Get().(*dto.ProductDto)
+	defer func() {
+		productAdd.Reset()
+		h.objPool.Put(productAdd)
+	}()
 	if err := swap.SwapTo(req, productAdd); err != nil {
 		return err
 	}
@@ -33,11 +43,60 @@ func (h *ProductHandler) AddProduct(ctx context.Context, req *product.ProductInf
 	return nil
 }
 
+// DeductInvetory 扣减订单库存事务补偿操作
 func (h *ProductHandler) DeductInvetory(ctx context.Context, in *product.OrderDetailReq, out *product.OrderProductResp) error {
-	err := h.ProductApplicationService.DeductInvetory(ctx, in)
+	if in.OrderId == 0 || len(in.Products) == 0 {
+		return errors.New(fmt.Sprintf("%d", in.OrderId), "data not found", http.StatusPreconditionFailed)
+	}
+	orderProductInvetoryDto := h.orderInvetoryPool.Get().(*dto.OrderProductInvetoryDto)
+	defer func() {
+		orderProductInvetoryDto.Reset()
+		h.orderInvetoryPool.Put(orderProductInvetoryDto)
+	}()
+	orderProductInvetoryDto.ConvertToOrderProductInvetoryDto(in)
+	err := h.ProductApplicationService.DeductInvetory(ctx, orderProductInvetoryDto)
 	if err != nil {
-		return err
+		return errors.New(fmt.Sprintf("%d", in.OrderId), fmt.Sprintf("deduct invetory error: %v", err), http.StatusPreconditionFailed)
 	}
 	out.StatusCode = "0000"
 	return nil
+}
+
+// DeductInvetoryRevert 扣减订单库存事务补偿操作
+func (h *ProductHandler) DeductInvetoryRevert(ctx context.Context, in *product.OrderDetailReq, out *product.OrderProductResp) error {
+	if in.OrderId == 0 || len(in.Products) == 0 {
+		return errors.New(fmt.Sprintf("%d", in.OrderId), "data not found", http.StatusPreconditionFailed)
+	}
+	orderProductInvetoryDto := h.orderInvetoryPool.Get().(*dto.OrderProductInvetoryDto)
+	defer func() {
+		orderProductInvetoryDto.Reset()
+		h.orderInvetoryPool.Put(orderProductInvetoryDto)
+	}()
+	orderProductInvetoryDto.ConvertToOrderProductInvetoryDto(in)
+	err := h.ProductApplicationService.DeductInvetoryRevert(ctx, orderProductInvetoryDto)
+	if err != nil {
+		return errors.New(fmt.Sprintf("%d", in.OrderId), fmt.Sprintf("deduct invetory revert error: %v", err), http.StatusPreconditionFailed)
+	}
+	out.StatusCode = "0000"
+	return nil
+}
+
+// NewProductHandler 创建Handler
+func NewProductHandler(appService service.IProductApplicationService) product.ProductHandler {
+	return &ProductHandler{
+		ProductApplicationService: appService,
+		objPool: sync.Pool{
+			New: func() interface{} {
+				return &dto.ProductDto{}
+			},
+		},
+		orderInvetoryPool: sync.Pool{
+			New: func() interface{} {
+				return &dto.OrderProductInvetoryDto{
+					ProductInvetory:     []*dto.OrderProductInvetoryItem{},
+					ProductSizeInvetory: []*dto.OrderProductSizeInvetoryItem{},
+				}
+			},
+		},
+	}
 }
