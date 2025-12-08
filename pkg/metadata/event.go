9b1a6dd5dbe187a6c8b0a4e3aad8a53d79077f73
wangjiawei1995 <zhanshen02154@gmail.com>
2025-12-08 10:14:52 +0800

    fix: 修复事件ID的Key错误问题
    fix #61

--- pkg/metadata/event.go
+++ pkg/metadata/event.go
@@ -5,7 +5,7 @@ import (
 	"go-micro.dev/v4/metadata"
 )
 
-const eventIdKey = "Micro-ID"
+const eventIdKey = "Event_id"
 
 // GetEventId 获取事件ID
 func GetEventId(ctx context.Context) (string, bool) {
