68f7fd834deeb0c3ed267045abb09beb67327c46
wangjiawei1995 <zhanshen02154@gmail.com>
2026-01-19 10:53:03 +0800

    refactor(分布式锁): 由ETCD改为Redis
    resolves #125, zhanshen02154@go-micro-service#41
    
    BREAKING CHANGE: - 将分布式锁组件从ETCD更改为Redis
    - 重构分布式锁接口

--- internal/infrastructure/context.go
+++ internal/infrastructure/context.go
@@ -20,15 +20,22 @@ type ServiceContext struct {
 }
 
 func NewServiceContext(conf *config.SysConfig, zapLogger gormlogger.Interface) (*ServiceContext, error) {
+	var err error
 	db, err := InitDB(conf.Database, zapLogger)
 	if err != nil {
 		return nil, err
 	}
-
-	// 加载ETCD分布式锁
-	lockMgr, err := NewEtcdLockManager(conf.Etcd)
+	defer func() {
+		if err != nil {
+			if sqlDb, err := db.DB(); err == nil {
+				sqlDb.Close()
+			}
+			logger.Error("failed to load service context: " + err.Error())
+		}
+	}()
+	// 加载Redis分布式锁
+	lockMgr, err := NewRedisLockManager(conf.Redis)
 	if err != nil {
-		logger.Error("failed to load lock manager: " + err.Error())
 		return nil, err
 	}
 	return &ServiceContext{
@@ -46,9 +53,9 @@ func (svc *ServiceContext) Close() {
 	if err := svc.closeDB(); err != nil {
 		logger.Error("close database error: " + err.Error())
 	}
-	// 关闭ETCD
-	if err := svc.closeEtcd(); err != nil {
-		logger.Error("close etcd error: " + err.Error())
+	// 关闭Redis分布式锁
+	if err := svc.closeLock(); err != nil {
+		logger.Error("close redis lock manager error: " + err.Error())
 	}
 }
 
@@ -65,30 +72,34 @@ func (svc *ServiceContext) closeDB() error {
 		logger.Error("Failed to close database instance: " + err.Error())
 		return err
 	} else {
-		logger.Info("GORM数据库连接已关闭")
+		logger.Info("Database instance closed")
 	}
 	return nil
 }
 
-// 关闭ETCD
-func (svc *ServiceContext) closeEtcd() error {
+// 关闭分布式锁
+func (svc *ServiceContext) closeLock() error {
 	err := svc.LockManager.Close()
 	if err != nil {
-		logger.Error("Failed to close etcd lock manager: " + err.Error())
+		logger.Error("Failed to close redis lock manager: " + err.Error())
 	} else {
-		logger.Info("ETCD lock manager closed")
+		logger.Info("Redis lock manager closed")
 	}
 	return err
 }
 
-// CheckHealth 检查是否健康
+// CheckHealth 健康检查
 func (svc *ServiceContext) CheckHealth() error {
 	sqlDB, err := svc.db.DB()
 	if err != nil {
 		return err
 	}
 	if err := sqlDB.Ping(); err != nil {
-		logger.Error("Failed to close database instance: " + err.Error())
+		logger.Error("Database instance unhealthy: " + err.Error())
+	}
+	if err := svc.LockManager.CheckHealth(); err != nil {
+		logger.Error("Redis lock manager unhealthy: " + err.Error())
+		return err
 	}
 	return nil
 }
